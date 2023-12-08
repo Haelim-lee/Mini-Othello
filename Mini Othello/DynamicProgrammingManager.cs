2cf4600676aa11ef7ac42863c435774762dbcfa8
gpfla8966 <gpfla8966@gmail.com>
2023-12-08 15:02:22 +0900

    First Commit

---
+++ Mini Othello/DynamicProgrammingManager.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+
+namespace Mini_Othello
+{
+	public class DynamicProgrammingManager
+	{
+		public Dictionary<int, float> StateValueFunction { get; set; } // 상태 가치 함수값들을 저장하기 위한 Dictionary
+		public float DiscountFactor = 0.9f;
+		public string stateValueFunctionFilePath = "StateValueFunction.json";
+
+		public DynamicProgrammingManager()
+		{
+			StateValueFunction = new Dictionary<int, float>();
+		}
+
+		public void UpdateByDynamicProgramming()
+		{
+			InitializeValueFunction();
+
+			ApplyDynamicProgramming();
+		}
+
+		public void InitializeValueFunction()
+		{
+			Console.Clear();
+			Console.WriteLine("동적 프로그래밍 시작");
+			Console.WriteLine("가치 함수 초기화");
+
+			StateValueFunction.Clear();
+			
+			var gameState = new GameState(); // 초기 게임 상태 생성
+			var boardStateKeyList = new List<int>(); // 게임 상태 후보 리스트 선언
+			boardStateKeyList.Add(gameState.BoardStateKey); // 초기 상태 게임 상태키를 후보 리스트에 추가
+			var mergedChildStateList = new List<int>();
+
+			while (true) // 루프 시작
+			{
+				mergedChildStateList.Clear(); // 처리가 끝난 자식 상태 리스트 비우기
+				foreach (int gameBoardKey in boardStateKeyList) // 게임 상태 후보 리스트에 있는 상태들에 대해
+				{
+					if (!StateValueFunction.ContainsKey(gameBoardKey)) // 가치 함수에 아직 포함되지 않았으면
+					{
+						StateValueFunction.Add(gameBoardKey, 0.0f); // 가치 함수에 추가
+						var processingGameState = new GameState(gameBoardKey); // 게임 상태 생성
+
+						if (!processingGameState.isFinalState()) // 게임 종료 상태가 아니면
+						{
+							var childStateList = new List<int>();
+							for (var i = GameParameters.ActionMinIndex; i <= GameParameters.ActionMaxIndex; i++)  // 모든 행동에 대해 루프를 수행
+							{
+								if (processingGameState.IsValidMove(i)) // 이 행동이 올바른 행동이면
+								{
+									var nextState = processingGameState.GetNextState(i); // 행동을 통해 전이해 간 다음 상태 구성
+									childStateList.Add(nextState.BoardStateKey); // 자식 상태 임시 후보 리스트에 추가
+								}
+							}
+							if (childStateList.Count == 0) // 만일 후보 리스트에 포함된 상태가 없으면
+							{
+								var nextState = processingGameState.GetNextState(0); // Pass 로 턴만 바뀐 상태 생성
+								childStateList.Add(nextState.BoardStateKey); // 임시 후보 리스트에 추가
+							}
+
+							// 임시 후보 리스트의 상태 중 자식 상태 리스트에 포함되어 있지 않은 상태를 자식 상태 리스트에 추가
+							mergedChildStateList.AddRange(childStateList.Where(e => !mergedChildStateList.Contains(e)));
+						}
+					}
+				}
+				if (mergedChildStateList.Count == 0) // 자식 상태 리스트에 상태가 없으면
+					break; // 루프 종료
+				else
+					boardStateKeyList = new List<int>(mergedChildStateList); // 게임 상태 후보 리스트를 자식 상태로 치환하고 루프 지속
+			}
+
+			Console.WriteLine(Environment.NewLine);
+			Console.WriteLine($"가치 함수 초기화 완료, 상태 {StateValueFunction.Count} 개");
+			Console.WriteLine(Environment.NewLine);
+			Console.Write("아무 키나 누르세요:");
+			Console.ReadLine();
+		}
+
+		public void ApplyDynamicProgramming()
+		{
+			Console.Clear();
+			Console.WriteLine("동적 프로그래밍 적용");
+			Console.WriteLine(Environment.NewLine);
+
+			var loopCount = 0;
+			var terminateLoop = false;
+
+			while (!terminateLoop)
+			{
+				var nextStateValueFunction = new Dictionary<int, float>(); // 업데이트되는 가치 함수값을 임시로 저장하기 위한 dictionary
+				var valueFunctionUpdateAmount = 0.0f; // 동적 프로그래밍 각 단계에서 함수값이 업데이트된 크기
+
+				foreach (KeyValuePair<int, float> valueFunctionEntry in StateValueFunction) // 가치 함수 업데이트 루프
+				{
+					var updatedValue = UpdateValueFunction(valueFunctionEntry.Key); // 가치 함수 업데이트 계산
+					var updatedAmount = Math.Abs(valueFunctionEntry.Value - updatedValue); // 업데이트 크기
+					nextStateValueFunction[valueFunctionEntry.Key] = updatedValue; // 가치 함수 업데이트
+
+					if (updatedAmount > valueFunctionUpdateAmount) // 루프를 돌면서 함수가 업데이트된 크기를 기록
+						valueFunctionUpdateAmount = updatedAmount;
+				}
+
+				StateValueFunction = new Dictionary<int, float>(nextStateValueFunction); // 가치 함수값을 임시 저장 가치 함수로 변경
+
+				loopCount++;
+				Console.WriteLine($"동적 프로그래밍 {loopCount}회 수행, 업데이트 오차 {valueFunctionUpdateAmount}");
+
+				if (valueFunctionUpdateAmount < 0.01f) // 업데이트 크기가 충분히 작으면 루프 종료
+					terminateLoop = true;
+			}
+
+			Console.WriteLine(Environment.NewLine);
+			Console.Write("아무 키나 누르세요:");
+			Console.ReadLine();
+		}
+
+		public float UpdateValueFunction(int gameStateKey)
+		{
+			var gameState = new GameState(gameStateKey); // 주어진 게임 상태 키에 대해 게임 상태 생성
+
+			if (gameState.isFinalState()) // 게임 종료 상태이면 함수값 0 반환
+				return 0.0f;
+
+			var actionExpectationList = new List<float>();
+
+			for (int i = GameParameters.ActionMinIndex; i <= GameParameters.ActionMaxIndex; i++)
+			{
+				if (gameState.IsValidMove(i)) // 이 행동이 올바른 행동이면
+				{
+					var nextState = gameState.GetNextState(i); // 행동을 통해 전이해 간 다음 상태 구성
+					var reward = nextState.GetReward(); // 다음 상태에서의 보상값 확인
+
+					var actionExpectation = reward + DiscountFactor * StateValueFunction[nextState.BoardStateKey]; // 행동 가치 함수값 계산
+
+					actionExpectationList.Add(actionExpectation); // 계산된 가치 함수값을 저장
+				}
+			}
+			if (actionExpectationList.Count == 0) // 만일 올바른 행동이 하나도 없는 상태라면
+			{
+				var nextState = gameState.GetNextState(0); // Pass를 통해 턴만 바뀐 상태에 대한 가치함수를 가져올 수 있도록 변경
+				var reward = nextState.GetReward(); // 다음 상태에서의 보상값 확인
+
+				var actionExpectation = reward + DiscountFactor * StateValueFunction[nextState.BoardStateKey]; // 행동 가치 함수값 계산
+
+				actionExpectationList.Add(actionExpectation); // 계산된 가치 함수값을 저장
+			}
+
+			if (gameState.NextTurn == 1) // 흑돌이 둘 차례이면 저장된 가치 함수값 중 최대값 반환
+				return actionExpectationList.Max();
+			else if (gameState.NextTurn == 2) // 백돌이 둘 차례이면 저장된 가치 함수값 중 최소값 반환
+				return actionExpectationList.Min();
+			return 0.0f;
+		}
+
+		public int GetNextMove(int boardStateKey)
+		{
+			// 주어진 게임 상태에 대해서 선택할 수 있는 행동 후보값을 구한 후,
+			var actionCandidates = GetNextMoveCandidate(boardStateKey);
+
+			if (actionCandidates.Count() == 0) // pass
+				return 0;
+
+			// 그 중 한 값을 랜덤하게 선택해서 반환
+			return actionCandidates.ElementAt(Utilities.random.Next(0, actionCandidates.Count()));
+		}
+
+		public IEnumerable<int> GetNextMoveCandidate(int boardStateKey)
+		{
+			var selectedExpectation = 0.0f;
+
+			var gameState = new GameState(boardStateKey); // 주어진 상태에 대한 게임 상태 생성
+			var actionCandidateDictionary = new Dictionary<int, float>();
+
+			for (var i = GameParameters.ActionMinIndex; i <= GameParameters.ActionMaxIndex; i++)
+			{
+				if (gameState.IsValidMove(i)) // 이 행동에 이 상태에 적용 가능한 올바른 행동인 경우
+				{
+					var nextState = gameState.GetNextState(i); //그 행동을 통해 전이해가는 상태를 구하고
+					var reward = nextState.GetReward(); // 그 전이해 간 상태에서의 보상값
+
+					var actionExpectation = reward + DiscountFactor * StateValueFunction[nextState.BoardStateKey]; // 행동 가치 함수값 계산
+
+					actionCandidateDictionary.Add(i, actionExpectation); // 행동과 그 행동에 대한 행동 가치 함수값을 저장
+				}
+			}
+
+			if (actionCandidateDictionary.Count == 0)
+				return new List<int>();
+
+			if (gameState.NextTurn == 1) // 흑돌 차례인 경우 저장된 행동 가치 함수값 중 최대값을 선택
+			{
+				selectedExpectation = actionCandidateDictionary.Select(e => e.Value).Max();
+			}
+			else if (gameState.NextTurn == 2) // 백돌 차례인 경우 저장된 행동 가치 함수값 중 최소값 선택
+			{
+				selectedExpectation = actionCandidateDictionary.Select(e => e.Value).Min();
+			}
+
+			// 선택한 가치 함수값을 가지는 행동들을 모두 모아서 반환
+			return actionCandidateDictionary.Where(e => e.Value == selectedExpectation).Select(e => e.Key);
+		}
+
+		public void SaveStateValueFunction()
+		{
+			// 가치 함수 저장
+			// Json Serilizer 적용 후 text 형태로 저장
+			var settings = new JsonSerializerSettings();
+			settings.Formatting = Formatting.Indented;
+
+			var stateValueFunctionInJson = JsonConvert.SerializeObject(StateValueFunction, settings);
+
+			File.WriteAllText(stateValueFunctionFilePath, stateValueFunctionInJson);
+
+			Console.Clear();
+			Console.WriteLine($"가치 함수가 파일 {stateValueFunctionFilePath}에 저장되었습니다.");
+			Console.WriteLine(Environment.NewLine);
+			Console.Write("아무 키나 누르세요:");
+			Console.ReadLine();
+		}
+
+		public void LoadStateValueFunction()
+		{
+			// 가치 함수 로드
+			// 텍스트 형태로 읽어온 후 Json Deserialize 적용
+			if (File.Exists(stateValueFunctionFilePath))
+			{
+				var stateValueFunctionInJson = File.ReadAllText(stateValueFunctionFilePath);
+
+				var settings = new JsonSerializerSettings();
+				settings.Formatting = Formatting.Indented;
+
+				StateValueFunction = JsonConvert.DeserializeObject<Dictionary<int, float>>(stateValueFunctionInJson, settings);
+
+				Console.Clear();
+				Console.WriteLine($"가치 함수가 파일 {stateValueFunctionFilePath}에서 로드되었습니다.");
+			}
+			else
+			{
+				Console.Clear();
+				Console.WriteLine($"가치 함수 파일 {stateValueFunctionFilePath}이 존재하지 않습니다.");
+			}
+			Console.WriteLine(Environment.NewLine);
+			Console.Write("아무 키나 누르세요:");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ Mini Othello/GameState.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Mini_Othello
+{
+	public class GameParameters
+	{
+		public static int ActionMinIndex = 1;
+		public static int ActionMaxIndex = 16;
+		public static int BoardRowCount = 4;
+		public static int BoardColCount = 4;
+		public static int BoardMaxIndex = 15;
+	}
+
+	public class GameState
+	{
+		public int[,] BoardState;
+		public int NextTurn;
+		public int BoardStateKey;
+		public int GameWinner;
+		public int NumOfBlack;
+		public int NumOfWhite;
+
+		public GameState()
+		{
+			// 초기 게임 상태로 클래스를 초기화
+			// 보드의 중앙에 흑돌과 백돌이 초기상태로 2개씩 놓여있고 흑돌 차례인 상태
+
+			BoardState = new int[,] { { 0, 0, 0, 0 }, { 0, 1, 2, 0 }, { 0, 2, 1, 0 }, { 0, 0, 0, 0} };
+			NextTurn = 1;
+
+			var boardStateKey = 0;
+			for (var i = 0; i <= GameParameters.BoardMaxIndex; i++)
+			{
+				boardStateKey = boardStateKey * 3;
+				boardStateKey = boardStateKey + BoardState[GetRowFromIndex(i), GetColFromIndex(i)];
+			}
+
+			BoardStateKey = boardStateKey* 3 + NextTurn;
+			GameWinner = 0;
+			NumOfBlack = 2;
+			NumOfWhite = 2;
+		}
+
+		public GameState(int boardStateKey)
+		{
+			// 주어진 게임 상태 키로부터 게임 상태를 생성하면서 클래스를 초기화
+
+			BoardState = new int[4, 4];
+			BoardStateKey = boardStateKey;
+			NextTurn = boardStateKey % 3;
+			GameWinner = 0;
+			PopulateBoard(boardStateKey / 3);
+		}
+
+		public void PopulateBoard(int boardState)
+		{
+			// 주어진 보드 상태 값을 3진수로 변환시키면서 보드 상태 생성
+
+			var boardValueProcessing = boardState;
+			NumOfBlack = 0;
+			NumOfWhite = 0;
+
+			for (var i = GameParameters.BoardMaxIndex; i >= 0; i--)
+			{
+				var boardValue = boardValueProcessing % 3;
+				boardValueProcessing = boardValueProcessing / 3;
+
+				BoardState[GetRowFromIndex(i), GetColFromIndex(i)] = boardValue;
+
+				if (boardValue == 1)
+					NumOfBlack++;
+				if (boardValue == 2)
+					NumOfWhite++;
+			}
+		}
+
+		public bool isFinalState()
+		{
+			// 게임이 끝난 상태인지 확인
+			GameWinner = 0;
+			NumOfWhite = 0;
+			NumOfBlack = 0;
+
+			for(var i = 0; i < GameParameters.BoardRowCount; i++)
+			{
+				for (var j = 0; j < GameParameters.BoardColCount; j++)
+				{
+					switch(BoardState[i, j])
+					{
+						case 1:
+							NumOfBlack++;
+							break;
+						case 2:
+							NumOfWhite++;
+							break;
+						default:
+							break;
+					}
+				}
+			}
+
+			// 흑돌, 백돌 모두 더 이상 수가 없는 당태이면 종료 상태로 판단
+			if(CountValidMoves(1) == 0 && CountValidMoves(2) == 0)
+			{
+				if (NumOfBlack > NumOfWhite) // 흑돌이 더 많으면 흑돌이 승자
+				{
+					GameWinner = 1;
+				}
+				else if (NumOfBlack < NumOfWhite) // 백돌이 더 많으면 백돌이 승자
+				{
+					GameWinner = 2;
+				}
+				return true;
+			}
+
+			return false;
+		}
+
+		public float GetReward()
+		{
+			// 보상값 함수
+			// 게임을 1이 이긴 상태이면 100, 2가 이긴 상태이면 -100, 그렇지 않으면 0 반환
+
+			if (isFinalState())
+			{
+				if (GameWinner == 1)
+					return 100.0f;
+				else if (GameWinner == 2)
+					return -100.0f;
+			}
+
+			return 0.0f;
+		}
+
+		public int CountValidMoves()
+		{
+			return CountValidMoves(NextTurn);
+		}
+
+		private int CountValidMoves(int turn)
+		{
+			// 현재 게임 상태에 대해 올바른 행동의 개수를 찾아보는 함수
+
+			var count = 0;
+			for (var i = GameParameters.ActionMinIndex; i <= GameParameters.ActionMaxIndex; i++)
+			{
+				// 모든 위치에 대해 올바른 행동인지 판단
+				if (IsValidMove(i, turn))
+					count++;
+			}
+			return count;
+		}
+
+		public bool IsValidMove(int move)
+		{
+			return IsValidMove(move, NextTurn);
+		}
+
+		public bool IsValidMove(int move, int turn)
+		{
+			// 현재 주어진 게임 상태에 대해 주어진 행동을 적용할 수 있는지 판단하는 함수
+			var index = move - 1;
+			var row = GetRowFromIndex(index);
+			var col = GetColFromIndex(index);
+
+			// 보드상에 주어진 위치가 비어 있는 곳이어야 하며
+			if (BoardState[row, col] == 0)
+			{
+				for(var incRow = -1; incRow <= 1; incRow++)
+				{
+					for (var incCol = -1; incCol <= 1; incCol++)
+					{
+						if (incRow != 0 || incCol != 0)
+						{
+							// 그 위치로부터 가로, 세로, 대각선 8개 방향으로 상대방의 돌을 넘실 수 있는 수가 있어야 한다
+							if (IsMoveValidInDirection(row, col, incRow, incCol, turn))
+								return true;
+						}
+					}
+				}
+				
+			}
+
+			return false;
+		}
+
+		public bool IsMoveValidInDirection(int targetRow, int targetCol, int incRow, int incCol, int turn)
+		{
+			int testingRow;
+			int testingCol;
+			int enemyStoneCount = 0;
+			int increment = 1;
+
+			// 주어진 위치 (targetRow, TargetCol)에서 주어진 방향 (incRow, incCol) 으로 진행하면서 상대방 돌을 넘길수 있는지 판단
+			while(true)
+			{
+				testingRow = targetRow + incRow * increment;
+				testingCol = targetCol + incCol * increment;
+
+				if (testingRow < 0 || testingRow >= GameParameters.BoardRowCount) // 인덱스가 보드 밖으로 나가면 안됨
+					return false;
+				if (testingCol < 0 || testingCol >= GameParameters.BoardColCount) // 인덱스가 보드 밖으로 나가면 안됨
+					return false;
+
+				int testingPlaceStone = BoardState[testingRow, testingCol];
+
+				if (testingPlaceStone == 0) // 진행 중에 빈칸이 있어서도 안됨
+					return false;
+				if (testingPlaceStone != turn) // 상대방 돌을 만나면 그 개수를 기록함
+					enemyStoneCount++;
+				if (testingPlaceStone == turn) // 인덱스가 보드 밖으로 나가거나 빈칸을 만나기 전에, 상대방 돌을 1개 이상 연속으로 만난 후 같은편 돌을 만나야 함
+				{
+					if (enemyStoneCount == 0)
+						return false;
+					else
+						return true;
+				}
+				increment++;
+			}
+		}
+
+		public void MakeMoveInDirection(int targetRow, int targetCol, int incRow, int incCol, int turn)
+		{
+			int testingRow;
+			int testingCol;
+			int increment = 1;
+
+			// 주어진 위치 (targetRow, TargetCol)에서 주어진 방향 (incRow, incCol) 으로 진행하면서 우리편 돌을 만날 때까지 상대방 돌을 우리돌로 바꿈
+			while (true)
+			{
+				testingRow = targetRow + incRow * increment;
+				testingCol = targetCol + incCol * increment;
+
+				int testingPlaceStone = BoardState[testingRow, testingCol];
+
+				if (testingPlaceStone != turn)
+				{
+					BoardState[testingRow, testingCol] = turn;
+					if(turn == 1)
+					{
+						NumOfBlack++;
+						NumOfWhite--;
+					}
+					else if (turn == 2)
+					{
+						NumOfBlack--;
+						NumOfWhite++;
+					}
+				}
+				if (testingPlaceStone == turn)
+					return;
+				increment++;
+			}
+		}
+
+		public GameState GetNextState(int move)
+		{
+			// 현재 게임 상태에 대해 주어진 행동을 취하여 전이되어 가는 게임 상태를 생성해서 반환
+			var nextState = new GameState(BoardStateKey);
+			nextState.MakeMove(move);
+			return nextState;
+		}
+
+		public void MakeMove(int move)
+		{
+			var boardStateKey = 0;
+
+			if (move != 0) // pass 가 아닌 경우
+			{
+				// 현재 게임 상태에 행동을 적용하는 함수
+				var index = move - 1;
+				var row = GetRowFromIndex(index);
+				var col = GetColFromIndex(index);
+
+				// 주어진 위치에 다음 차례에 둘 돌을 놓은 후
+				BoardState[row, col] = NextTurn;
+				if (NextTurn == 1)
+					NumOfBlack++;
+				else if (NextTurn == 2)
+					NumOfWhite++;
+
+				// 가로, 세로, 대각선 8개 방향으로 진행하면서
+				for (var incRow = -1; incRow <= 1; incRow++)
+				{
+					for (var incCol = -1; incCol <= 1; incCol++)
+					{
+						if (incRow != 0 || incCol != 0)
+							if (IsMoveValidInDirection(row, col, incRow, incCol, NextTurn)) // 상대방 돌을 우리 돌로 바꿀 수 있는 경우가 존재하면
+								MakeMoveInDirection(row, col, incRow, incCol, NextTurn); // 우리 돌로 바꿔줌
+					}
+				}
+
+				for (int i = 0; i <= GameParameters.BoardMaxIndex; i++) // 게임 보드 상태키 재설정하기 위한 보드 상태의 3진수화
+				{
+					boardStateKey = boardStateKey * 3;
+					boardStateKey = boardStateKey + BoardState[GetRowFromIndex(i), GetColFromIndex(i)];
+				}
+			}
+			else // Pass 한 경우에는 턴 정보만 바꾸기 위해 보드 상태키에서 턴정보를 뺀 보드 상태 3진수값 구해두기
+			{
+				boardStateKey = BoardStateKey / 3;
+			}
+
+			// 턴 변경
+			if (NextTurn == 1)
+				NextTurn = 2;
+			else if (NextTurn == 2)
+				NextTurn = 1;
+
+			// 턴정보 추가하여 보드 상태키 업데이트
+			BoardStateKey = boardStateKey * 3 + NextTurn;
+		}
+
+		public void DisplayBoard(int turnCount, int lastMove, GamePlayer blackPlayer, GamePlayer whitePlayer)
+		{
+			// 화면에 현재 게임 상태를 출력하는 함수. 게임 진행 과정에서 사용됨
+			Console.Clear();
+
+			Console.WriteLine($"X: {blackPlayer}, O: {whitePlayer}");
+			Console.WriteLine();
+			Console.WriteLine($"게임 턴: {turnCount}, {GetTurnMark()} 차례입니다.");
+			Console.Write($"Black:{NumOfBlack}, White:{NumOfWhite}, ");
+
+			if (turnCount != 0)
+			{
+				if (lastMove != 0)
+					Console.WriteLine($" 지난 행동, Row: {GetRowFromIndex(lastMove - 1)}, Column: {GetColFromIndex(lastMove - 1)}");
+				else
+					Console.WriteLine($" 지난 행동, Pass");
+			}
+			else
+			{
+				Console.WriteLine();
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("    +-------+-------+-------+-------+");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine($"    |   {GetGameBoardValue(0, 0)}   |   {GetGameBoardValue(0, 1)}   |   {GetGameBoardValue(0, 2)}   |   {GetGameBoardValue(0, 3)}   |");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine("    +-------+-------+-------+-------+");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine($"    |   {GetGameBoardValue(1, 0)}   |   {GetGameBoardValue(1, 1)}   |   {GetGameBoardValue(1, 2)}   |   {GetGameBoardValue(1, 3)}   |");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine("    +-------+-------+-------+-------+");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine($"    |   {GetGameBoardValue(2, 0)}   |   {GetGameBoardValue(2, 1)}   |   {GetGameBoardValue(2, 2)}   |   {GetGameBoardValue(2, 3)}   |");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine("    +-------+-------+-------+-------+");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine($"    |   {GetGameBoardValue(3, 0)}   |   {GetGameBoardValue(3, 1)}   |   {GetGameBoardValue(3, 2)}   |   {GetGameBoardValue(3, 3)}   |");
+			Console.WriteLine("    |       |       |       |       |");
+			Console.WriteLine("    +-------+-------+-------+-------+");
+
+
+			isFinalState();
+			Console.WriteLine(Environment.NewLine);
+			if(isFinalState())
+			{
+				switch (GameWinner)
+				{
+					case 1:
+						Console.WriteLine("X 가 이겼습니다!!");
+						break;
+					case 2:
+						Console.WriteLine("O 가 이겼습니다!!");
+						break;
+					default:
+						Console.WriteLine("게임이 비겼습니다!!");
+						break;
+				}
+			}
+			else
+			{
+				Console.WriteLine("게임 진행중입니다!!");
+			}
+		}
+
+		private string GetTurnMark()
+		{
+			return NextTurn == 1 ? "X" : "O";
+		}
+
+		private string GetGameBoardValue(int row, int col)
+		{
+			switch (BoardState[row, col])
+			{
+				case 1:
+					return "X";
+				case 2:
+					return "O";
+				default:
+					return "+";
+			}
+		}
+
+		private int GetRowFromIndex(int index)
+		{
+			return index / GameParameters.BoardRowCount;
+		}
+
+		private int GetColFromIndex(int index)
+		{
+			return index % GameParameters.BoardColCount;
+		}
+	}
+}
